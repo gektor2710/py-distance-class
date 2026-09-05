037d8c7b7d4a409b8a8b7d1c3fb12317f35574f2
Mykola Kobzarov <gektor2710@gmail.com>
2026-09-05 20:54:49 +0200

    Implement Distance class with magic methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,51 @@
+from builtins import isinstance, round, float, bool
+
+
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers"
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __iadd__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other: Distance) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: Distance) -> Distance:
+        return Distance(round(self.km / other, 2))
+
+    def _get_value(self, other: Distance) -> float:
+        if isinstance(other, Distance):
+            return other.km
+        return other
+
+    def __lt__(self, other: Distance) -> bool:
+        return self.km < self._get_value(other)
+
+    def __gt__(self, other: Distance) -> bool:
+        return self.km > self._get_value(other)
+
+    def __eq__(self, other: Distance) -> bool:
+        return self.km == self._get_value(other)
+
+    def __le__(self, other: Distance) -> bool:
+        return self.km <= self._get_value(other)
+
+    def __ge__(self, other: Distance) -> bool:
+        return self.km >= self._get_value(other)
