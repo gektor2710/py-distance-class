b6f06bb447dedd52cb280ff0fc681fbe96ab5a17
Mykola Kobzarov <gektor2710@gmail.com>
2026-09-06 17:16:16 +0200

    Fix, __add__

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
